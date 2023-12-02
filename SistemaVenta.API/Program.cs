2f621590f20943628cd3510fe063e6c205dd1ae2
CarlosCode502 <carloshl.code21@gmail.com>
2023-12-02 13:01:27 -0600

    Agregando el connection string al appsetings (vstudio recomienda no usuarla en el dbContext) min 18.50 p2
    e Inyectando el servicio
    Para poder acceder al servico es necesario usar los métodos de extension (this IServiceCollection) min 26.44

--- SistemaVenta.API/Program.cs
+++ SistemaVenta.API/Program.cs
@@ -1,3 +1,5 @@
+using SistemaVenta.IOC; //Agregar la referencia para poder habilitar el servicio
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Para poder acceder al servico es necesario usar los métodos de extension (this IServiceCollection) min 26.44 
+builder.Services.InyectarDependencias(builder.Configuration); //para poder obtener la cadena desde appsettings 27.01
 
 var app = builder.Build();
 
---
+++ SistemaVenta.IOC/Dependencias.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBContext;
+
+namespace SistemaVenta.IOC
+{
+    
+    /// <summary>
+    /// Inyección de dependencias clase para agregar las dependencias
+    /// </summary>
+    public static class Dependencias
+    {
+        //Método que va recibir un servicio de colecciones para inyectar las dependencias (Método de extensión)
+        //IService Collection es un servicio que pertenece a .net se va a agregar el método
+        //Pasamos el iconfiguration
+        public static void InyectarDependencias(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Agregar el contexto de la base de datos (Como existe ref a DAL es posible acceder al contexto de la BD)
+            //Estamos asignando nuevamente la cadena de conexión al contexto BdSysventaAngNetContext (min 24.50 aprox)
+            services.AddDbContext<BdSysventaAngNetContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("cadenaSQL"));
+            });
+        }
+    }
+}
