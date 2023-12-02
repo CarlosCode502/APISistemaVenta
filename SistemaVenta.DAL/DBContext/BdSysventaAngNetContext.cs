c593c2b10076c6dc17c968e2d056329dd5c3be9d
CarlosCode502 <carloshl.code21@gmail.com>
2023-12-02 12:38:05 -0600

    Agregando el connection string al appsetings (vstudio recomienda no usuarla en el dbContext) min 18.50 p2

--- SistemaVenta.DAL/DBContext/BdSysventaAngNetContext.cs
+++ SistemaVenta.DAL/DBContext/BdSysventaAngNetContext.cs
@@ -14,6 +14,7 @@ public partial class BdSysventaAngNetContext : DbContext
     public BdSysventaAngNetContext(DbContextOptions<BdSysventaAngNetContext> options)
         : base(options)
     {
+
     }
 
     public virtual DbSet<TblCategoria> TblCategorias { get; set; }
@@ -34,9 +35,12 @@ public partial class BdSysventaAngNetContext : DbContext
 
     public virtual DbSet<TblVenta> TblVenta { get; set; }
 
+    //Se reemplazo la cadena de conexión (se declaro en appsetings.json) min 19.20
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local); DataBase=BD_SYSVENTA_ANG_NET; Trusted_Connection=True; TrustServerCertificate=True;");
+    {
+        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        //        => optionsBuilder.UseSqlServer("");
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
