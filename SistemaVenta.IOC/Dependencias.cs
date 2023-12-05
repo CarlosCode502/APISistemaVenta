0c0d3e247db6264d83e2570ccf28b2525df6d117
CarlosCode502 <carloshl.code21@gmail.com>
2023-12-05 15:49:18 -0600

    Parte 5 (edit final) Agregando todos los servicios e agregando las dependencias.

--- SistemaVenta.IOC/Dependencias.cs
+++ SistemaVenta.IOC/Dependencias.cs
@@ -10,6 +10,8 @@ using SistemaVenta.DAL.Repositorios;
 
 //Usar las dependencias de la clase AutoMapperProfile 49.30 parte 4
 using SistemaVenta.Utility;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.BLL.Servicios;
 
 namespace SistemaVenta.IOC
 {
@@ -44,6 +46,17 @@ namespace SistemaVenta.IOC
             //Agregar dependencia de automapper con todos los mapeos
             //De las claes a DTO y DTO a las clases (modelos)
             services.AddAutoMapper(typeof(AutoMapperProfile));
+
+            //Agregando las dependencias de los métodos / servicios min 01.26.10 parte 5
+            //Interfaz, Clase que la implementa
+            services.AddScoped<IRolService, RolService>();
+            services.AddScoped<IUsuarioService, UsuarioService>();
+            services.AddScoped<ICategoriaService, CategoriaService>();
+            services.AddScoped<IProductoService, ProductoService>();
+            services.AddScoped<IVentaService, VentaService>();
+            services.AddScoped<IDashBoardService, DashBoardService>();
+            services.AddScoped<IMenuService, MenuService>();
+
         }
     }
 }
